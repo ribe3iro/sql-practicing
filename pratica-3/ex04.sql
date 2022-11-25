af6b65858b5864b1250ca5637fb85580b7987675
João Pedro <joaopedrorib@hotmail.com>
2022-11-25 01:36:21 -0300

    fix(pratica-3): atualizando consulta 4

--- pratica-3/ex04.sql
+++ pratica-3/ex04.sql
@@ -1,11 +1,8 @@
 SELECT j.classico, COUNT(p.data) AS nro_partidas
-FROM partida p RIGHT JOIN joga j ON p.time1 = j.time1 AND p.time2 = j.time2
-WHERE EXTRACT( MONTH FROM p.data ) IN (1, 2) AND j.classico IS NOT NULL
-GROUP BY j.classico
-;
-
-SELECT j.classico, COUNT(CASE WHEN EXTRACT( MONTH FROM p.data ) IN (1, 2) THEN 1 END) AS nro_partidas
-FROM partida p RIGHT JOIN joga j ON p.time1 = j.time1 AND p.time2 = j.time2
+FROM partida p RIGHT JOIN joga j ON 
+     p.time1 = j.time1 AND 
+     p.time2 = j.time2 AND 
+     EXTRACT( MONTH FROM p.data ) IN (1, 2)
 WHERE j.classico IS NOT NULL
 GROUP BY j.classico
 ;
