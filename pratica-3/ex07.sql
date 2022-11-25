afe85eff2a21735a266c5df27263a54629462d01
João Pedro <joaopedrorib@hotmail.com>
2022-11-25 01:35:41 -0300

    feat(pratica-3): adicionando novas consultas

--- pratica-3/ex07.sql
+++ pratica-3/ex07.sql
@@ -1,10 +1,13 @@
-SELECT t1.nome, COUNT(*) AS nro_sem_gols
-FROM time t1 JOIN partida p ON t1.nome = p.time1
-WHERE t1.nome IN (SELECT DISTINCT t_.nome
+SELECT t.nome, COUNT(*) AS partidas_sem_gols
+FROM time t JOIN partida p ON t.nome = p.time1 OR t.nome = p.time2
+WHERE t.nome IN (SELECT DISTINCT t_.nome
                  FROM partida p_ JOIN joga j_ ON p_.time1 = j_.time1 AND p_.time2 = j_.time2
                                  JOIN time t_ ON t_.nome = p_.time1 OR t_.nome = p_.time2
                  WHERE t_.tipo = 'PROFISSIONAL' AND j_.classico = 'S')
-AND REGEXP_LIKE(p.placar, '[0]{1,2}\X[[:digit:]]{1,2}')
-GROUP BY t1.nome
+AND (
+    (t.nome = p.time1 AND REGEXP_LIKE(p.placar, '^(0|00)\X[[:digit:]]{1,2}')) OR
+    (t.nome = p.time2 AND REGEXP_LIKE(p.placar, '[[:digit:]]{1,2}\X(0|00)$'))
+    )
+GROUP BY t.nome
 HAVING COUNT(*) >= 2
 ;
