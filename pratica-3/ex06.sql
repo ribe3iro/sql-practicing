f68915292d35970a9cf3943868435e6f7a460aeb
João Pedro <joaopedrorib@hotmail.com>
2022-11-29 00:07:06 -0300

    feat(pratica-3): adicionando consulta equivalente (ex-06)

--- pratica-3/ex06.sql
+++ pratica-3/ex06.sql
@@ -4,3 +4,12 @@ FROM joga j JOIN partida p ON j.time1 = p.time1 AND j.time2 = p.time2
 GROUP BY t.nome, t.estado, t.saldo_gols, EXTRACT( YEAR FROM p.data )
 ORDER BY t.nome, ano
 ;
+
+-- SOLUÇÃO EQUIVALENTE
+
+SELECT t.nome, t.estado, t.saldo_gols, EXTRACT( YEAR FROM p.data ) AS ano, COUNT(j.time1) AS qntd_classicos
+FROM joga j RIGHT JOIN partida p ON (j.time1 = p.time1 AND j.time2 = p.time2) AND j.classico = 'S'
+            JOIN time t ON t.nome = p.time1 OR t.nome = p.time2
+GROUP BY t.nome, t.estado, t.saldo_gols, EXTRACT( YEAR FROM p.data )
+ORDER BY t.nome, ano
+;
